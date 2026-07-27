b1e8ea2a71ec58776052b728c010a924802b75e7
Mr Rabbit <rabbitmaid@proton.me>
2026-07-27 15:11:34 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+          <p>This is a simple paragraph.</p>
+      </section>
+    </>
+  )
+}
+
+export default App
